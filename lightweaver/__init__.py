94a0e0cabad8eece948ea7c587e49c587f683e6d
cmo <c.osborne.1@research.gla.ac.uk>
2020-04-02 16:38:37 +0100

    Added __init__.py

---
+++ lightweaver/__init__.py
@@ -0,0 +1,7 @@
+from .atmosphere import Atmosphere, ScaleType, BoundaryCondition
+from .atomic_set import SpectrumConfiguration, RadiativeSet, lte_pops, hminus_pops
+from .atomic_table import atomic_weight_sort, AtomicTable, get_global_atomic_table, set_global_atomic_table
+from .constants import *
+from .molecule import MolecularTable
+from .multi import read_multi_atmos
+from .utils import NgOptions, InitialSolution, voigt_H, planck, gaunt_bf, ConvergenceError, ExplodingMatrixError, get_data_path, get_default_molecule_path, vac_to_air, air_to_vac, convert_specific_intensity, CrswIterator
