a972c3b3f7e498b225eeabdc867c10d5662a8744
cmo <c.osborne.1@research.gla.ac.uk>
2020-04-23 19:48:14 +0100

    First crack at time-dependent NR, currently assuming theta=1. Interface isn't tidy at all. Looks promising in first tests. Significantly bigger difference in n_e compared to direct differencing on Judge test. Should fix the weird sign of F and dF throughout (see notes)

--- lightweaver/__init__.py
+++ lightweaver/__init__.py
@@ -5,7 +5,7 @@ from .constants import *
 from .molecule import MolecularTable
 from .multi import read_multi_atmos
 from .utils import NgOptions, InitialSolution, voigt_H, planck, gaunt_bf, ConvergenceError, ExplodingMatrixError, get_data_path, get_default_molecule_path, vac_to_air, air_to_vac, convert_specific_intensity, CrswIterator, UnityCrswIterator
-from .nr_update import nr_post_update, F
+from .nr_update import nr_post_update, F, Ftd
 from .LwCompiled import LwContext
 
 def Context(*args, **kwargs):
@@ -13,4 +13,5 @@ def Context(*args, **kwargs):
     ctx = LwContext(*args, **kwargs)
     ctx.nr_post_update = types.MethodType(nr_post_update, ctx)
     ctx.F = types.MethodType(F, ctx)
+    ctx.Ftd = types.MethodType(Ftd, ctx)
     return ctx
--- lightweaver/nr_update.py
+++ lightweaver/nr_update.py
@@ -2,19 +2,48 @@ import numpy as np
 from .atomic_set import lte_pops
 from scipy.linalg import solve
 
-def F(self, k, backgroundNe=0.0):
+def Ftd(self, k, dt, nPrev, backgroundNe=0.0, atoms=None):
     Nlevel = 0
-    for atom in self.activeAtoms:
+    if atoms is None:
+        atoms = self.activeAtoms
+
+    for atom in atoms:
         Nlevel += atom.Nlevel
     Neqn = Nlevel + 1
 
     stages = [np.array([l.stage for l in atom.atomicModel.levels]) 
-              for atom in self.activeAtoms]
+              for atom in atoms]
 
     F = np.zeros(Neqn)
     F[-1] = self.atmos.ne[k]
     start = 0
-    for idx, atom in enumerate(self.activeAtoms):
+    theta = 1.0
+    for idx, atom in enumerate(atoms):
+        F[start:start+atom.Nlevel] = theta * dt * (atom.Gamma[:, :, k] @ atom.n[:, k]) \
+                                      - (atom.n[:, k] - nPrev[idx][:, k])
+        F[start + atom.Nlevel - 1] = np.sum(atom.n[:, k]) - atom.nTotal[k]
+        F[-1] -= stages[idx] @ atom.n[:, k]
+        start += atom.Nlevel
+    F[-1] -= backgroundNe
+
+    return F
+
+def F(self, k, backgroundNe=0.0, atoms=None):
+    Nlevel = 0
+    if atoms is None:
+        atoms = self.activeAtoms
+
+    for atom in atoms:
+        Nlevel += atom.Nlevel
+    Neqn = Nlevel + 1
+
+    stages = [np.array([l.stage for l in atom.atomicModel.levels]) 
+              for atom in atoms]
+
+    F = np.zeros(Neqn)
+    F[-1] = self.atmos.ne[k]
+    start = 0
+    for idx, atom in enumerate(atoms):
         F[start:start+atom.Nlevel] = -(atom.Gamma[:, :, k] @ atom.n[:, k])
         F[start + atom.Nlevel - 1] = np.sum(atom.n[:, k]) - atom.nTotal[k]
         F[-1] -= stages[idx] @ atom.n[:, k]
@@ -24,21 +53,30 @@ def F(self, k, backgroundNe=0.0):
     return F
 
 
-def nr_post_update(self, fdCollisionRates=True):
+def nr_post_update(self, fdCollisionRates=True, hOnly=False, timeDependentData=None):
     assert self.activeAtoms[0].atomicModel.name.startswith('H')
     crswVal = self.crswCallback.val
 
+    timeDependent = (timeDependentData is not None)
+    atoms = self.activeAtoms[:1] if hOnly else self.activeAtoms
+
     Nlevel = 0
-    for atom in self.activeAtoms:
+    for atom in atoms:
         Nlevel += atom.Nlevel
     Neqn = Nlevel + 1
 
     Nspace = self.atmos.Nspace
     stages = [np.array([l.stage for l in atom.atomicModel.levels]) 
-              for atom in self.activeAtoms]
+              for atom in atoms]
     
+
+    if hOnly:
+        backgroundAtoms = self.arguments['spect'].radSet.atoms[1:] 
+    else:
+        backgroundAtoms = self.arguments['spect'].radSet.passiveAtoms
+
     backgroundNe = np.zeros_like(self.atmos.ne)
-    for idx, atomModel in enumerate(self.arguments['spect'].radSet.passiveAtoms):
+    for idx, atomModel in enumerate(backgroundAtoms):
         lteStages = np.array([l.stage for l in atomModel.levels]) 
         atom = self.arguments['eqPops'].atomicPops[atomModel.name]
         backgroundNe += (lteStages[:, None] * atom.n[:, :]).sum(axis=0)
@@ -47,7 +85,7 @@ def nr_post_update(self, fdCollisionRates=True):
 
     if fdCollisionRates:
         dC = []
-        for atom in self.activeAtoms:
+        for atom in atoms:
             atom.compute_collisions(fillDiagonal=True)
             Cprev = np.copy(atom.C)
             pertSize = 1e-4
@@ -67,23 +105,39 @@ def nr_post_update(self, fdCollisionRates=True):
     maxk = -1
     dF = np.zeros((Neqn, Neqn))
     Fnew = np.zeros(Neqn)
+    theta = 1.0
     for k in range(Nspace):
         dF[...] = 0.0
-        Fg = self.F(k, backgroundNe[k])
+        if timeDependent:
+            Fg = self.Ftd(k, dt=timeDependentData['dt'], 
+                          nPrev=timeDependentData['nPrev'], backgroundNe=backgroundNe[k])
+        else:
+            Fg = self.F(k, backgroundNe=backgroundNe[k], atoms=atoms)
 
         start = 0
-        for idx, atom in enumerate(self.activeAtoms):
+        for idx, atom in enumerate(atoms):
             Nlevel = atom.Nlevel
             dF[start:start+Nlevel, start:start+Nlevel] = -atom.Gamma[:, :, k]
+            if timeDependent:
+                dF[start:start+Nlevel, start:start+Nlevel] *= -theta * timeDependentData['dt']
+                dF[start:start+Nlevel, start:start+Nlevel] -= np.eye(Nlevel)
+
             for t in atom.trans:
                 if t.type == 'Continuum':
                     # dF[start + t.i, Neqn-1] -= (t.Rji[k] / self.atmos.ne[k]) * atom.n[t.j, k]
                     preconRji = atom.Gamma[t.i, t.j, k] - crswVal * atom.C[t.i, t.j, k]
-                    dF[start + t.i, Neqn-1] -= (preconRji / self.atmos.ne[k]) * atom.n[t.j, k]
+                    entry = -(preconRji / self.atmos.ne[k]) * atom.n[t.j, k]
+                    if timeDependent:
+                        entry *= -theta * timeDependentData['dt']
+                    dF[start + t.i, Neqn-1] += entry
+
 
             if fdCollisionRates:
                 for i in range(Nlevel):
-                    dF[start + i, Neqn-1] -= dC[idx][i, :, k] @ atom.n[:, k]
+                    entry = -dC[idx][i, :, k] @ atom.n[:, k]
+                    if timeDependent:
+                        entry *= -theta * timeDependentData['dt']
+                    dF[start + i, Neqn-1] += entry
 
             dF[start+Nlevel-1, :] = 0.0
             dF[start+Nlevel-1, start:start+Nlevel] = 1.0
@@ -106,14 +160,14 @@ def nr_post_update(self, fdCollisionRates=True):
         #     # raise ValueError
 
         start = 0
-        for atom in self.activeAtoms:
+        for atom in atoms:
             atom.n[:, k] += update[start:start+atom.Nlevel]
             start += atom.Nlevel
         self.atmos.ne[k] += update[-1]
 
         Fnew[:] = 0.0
         start = 0
-        for atom in self.activeAtoms:
+        for atom in atoms:
             Fnew[start:start+atom.Nlevel] = atom.n[:, k]
             start += atom.Nlevel
         Fnew[-1] = self.atmos.ne[k]
